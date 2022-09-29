a869a4903ae837070e2e2152d7ac499cdcc1d7eb
Noah Saso <noahsaso@gmail.com>
2022-09-29 16:44:22 -0700

    Fixed eslint config for typescript.

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -1,46 +1,70 @@
 // @ts-check
 
+const fs = require('fs')
+const path = require('path')
+
+const tsConfig = fs.existsSync('tsconfig.json')
+  ? path.resolve('tsconfig.json')
+  : fs.existsSync('types/tsconfig.json')
+  ? path.resolve('types/tsconfig.json')
+  : undefined
+
 /** @type {import("eslint").Linter.Config} */
 const eslintConfig = {
-  parser: '@typescript-eslint/parser',
   extends: ['plugin:prettier/recommended'],
-  plugins: ['@typescript-eslint', 'import', 'unused-imports'],
-  rules: {
-    'no-unused-vars': ['off'],
-    eqeqeq: ['error'],
-    '@typescript-eslint/no-unused-vars': ['off'],
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal'],
-        'newlines-between': 'always',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-      },
-    ],
-    'import/no-duplicates': 'error',
-    'sort-imports': [
-      'error',
-      {
-        // Let eslint-plugin-import handle declaration groups above.
-        ignoreDeclarationSort: true,
-        // Sort within import statements.
-        ignoreMemberSort: false,
-      },
-    ],
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-      },
-    ],
+  parserOptions: {
+    ecmaVersion: 'latest',
   },
+  env: {
+    es6: true,
+  },
+  overrides: [
+    {
+      files: ['**/*.d.ts', '**/*.ts', '**/*.tsx'],
+      excludedFiles: ['**/node_modules/**'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint', 'import', 'unused-imports'],
+      parserOptions: {
+        project: tsConfig,
+      },
+      rules: {
+        'no-unused-vars': ['off'],
+        eqeqeq: ['error'],
+        '@typescript-eslint/no-unused-vars': ['off'],
+        'import/order': [
+          'error',
+          {
+            groups: ['builtin', 'external', 'internal'],
+            'newlines-between': 'always',
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+          },
+        ],
+        'import/no-duplicates': 'error',
+        'sort-imports': [
+          'error',
+          {
+            // Let eslint-plugin-import handle declaration groups above.
+            ignoreDeclarationSort: true,
+            // Sort within import statements.
+            ignoreMemberSort: false,
+          },
+        ],
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+          'warn',
+          {
+            vars: 'all',
+            varsIgnorePattern: '^_',
+            args: 'after-used',
+            argsIgnorePattern: '^_',
+          },
+        ],
+      },
+    },
+  ],
 }
 
 module.exports = eslintConfig
